320cefea1e43e56aa9fb3df01e67a20718a8a44b
Ken Pu <ken.pu@uoit.ca>
2013-10-27 15:38:19 -0400

    sample: run sql

--- sample-jdbc/Sample.java
+++ sample-jdbc/Sample.java
@@ -11,8 +11,43 @@ public class Sample {
         System.out.println("Driver : " + className);
         System.out.println("URL    : " + url);
         try {
+
             connection = DriverManager.getConnection(url);
             System.out.println("Connection made successfully.");
+
+            Statement statement = connection.createStatement();
+
+            String sql;
+
+            sql = "drop table if exists enrolment";
+            statement.executeUpdate(sql);
+
+            sql = "create table enrolment(name string, course string, grade float)";
+            statement.executeUpdate(sql);
+
+            statement.executeUpdate("insert into enrolment values('Mary', 'Computer Science', 87.0)");
+            statement.executeUpdate("insert into enrolment values('John', 'Computer Science', 83.5)");
+
+            sql = "select * from enrolment";
+            ResultSet result = statement.executeQuery(sql);
+
+            while(result.next()) {
+                String name = result.getString(1);
+                String course = result.getString(2);
+                float  grade  = result.getFloat(3);
+                System.out.printf("%s takes %s, and received %f\n", name, course, grade);
+            }
+            result.close();
+
+            sql = "select course, avg(grade) from enrolment group by course";
+            result = statement.executeQuery(sql);
+            while(result.next()) {
+                String course = result.getString(1);
+                float  grade  = result.getFloat(2);
+                System.out.printf("[%s] average = %.2f\n", course, grade);
+            }
+            result.close();
+
         } catch(SQLException e) {
             System.out.println(e.getMessage());
         } finally {
